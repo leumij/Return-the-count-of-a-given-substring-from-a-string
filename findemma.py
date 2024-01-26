6dee37795c52b98c6c00f4df457b5499b7869a86
leumij <cjimuel74@gmail.com>
2024-01-26 17:40:00 +0800

    count emma

--- findemma.py
+++ findemma.py
@@ -1,3 +1,10 @@
 # Write a program to find how many times substring “Emma” appears in the given string.
 
+#pseudocode
+
+# Given
+str_x = "Emma is good developer. Emma is a writer"
+
 # Use count method to str 
+use = str_x.count("Emma")
+print(use)
